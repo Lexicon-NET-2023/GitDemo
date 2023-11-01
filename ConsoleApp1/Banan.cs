91f000d7c6fdd7adb00b0e2e5c1830127557b377
Jonathan Krall <jonathan.krall@lexicon.se>
2023-11-01 14:41:49 +0100

    Default value for banan

--- ConsoleApp1/Banan.cs
+++ ConsoleApp1/Banan.cs
@@ -11,6 +11,6 @@ namespace ConsoleApp1
 
         public int Id { get; set; }
         public  string Name { get; set; }
-        public string Description { get; set; }
+        public string Description { get; set; } = "Bendy Berry";
     }
 }
